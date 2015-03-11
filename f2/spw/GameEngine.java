c0e62ae8b1cefa8b24857702f78097bef7743028
yungsathit <baichumhad08@gmail.com>
2015-03-12 00:17:35 +0700

    GetSi

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -62,7 +62,7 @@ public class GameEngine implements KeyListener, GameReporter{
 			if(!e.isAlive()){
 				e_iter.remove();
 				gp.sprites.remove(e);
-				score += 100;
+				score += 10000;
 			}
 		}
 		
